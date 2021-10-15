2bf49028df5a63cd78ac79098e0f4bbd3afd6035
Kashish Goyal <kashishgoyal471@gmail.com>
2021-10-15 15:26:12 +0530

    Initial Commit

---
+++ src/resumeData.js
@@ -0,0 +1,126 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Kashish Goyal",
+    "role": "Frontend Developer and Programmer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I am an enthusiastic person and I always like to learn something new every moment.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/kashishgoyalindia/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/the-kashish-goyal",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/KashishG471",
+          "className":"fa fa-twitter"
+        },
+        {
+          "name":"gmail",
+          "url":"mailto:kashishgoyal471@gmail.com",
+          "className":"fa fa-mail-forward"
+        },
+        {
+          "name":"Instagram",
+          "url":"https://www.instagram.com/_kashishhh._/",
+          "className":"fa fa-instagram"
+        }
+      ],
+    "aboutme":"I am currently a pre-final year student at The Indian Institute of Information Technology, Bhopal and pursuing my B.Tech from here. I am a self taught competitive programmer and full stack developer currently diving deeper into conventional programming and Web development. I have good team work and leadership qualities. Smart worker and hard worker.",
+    "address":"India",
+    "website":"https://rbhatia46.github.io",
+    "education":[
+      {
+        "UniversityName":"The Indian Institute of Information Technology, Bhopal",
+        "specialization":"Computer Science Engineering",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2023",
+        "Achievements":""
+      },
+      {
+        "UniversityName":"AVMD Institute, Agra",
+        "specialization":"Senior Secondary School",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2018",
+        "Achievements":""
+      },
+      {
+        "UniversityName":"Delhi Public School, Agra", 
+        "specialization":"Secondary School",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2016",
+        "Achievements":""
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"CPP"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
